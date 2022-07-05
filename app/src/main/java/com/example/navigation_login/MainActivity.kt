703c789ef9a229d9ed0a346049d708074a197efd
Alumno <afasfa>
2022-07-05 17:47:49 +0200

    Commit second. Uso del bottomnavigationmenu. Falta hacerlo mas elegante en un solo tv y quizas ponerle un toast en caso de que no se completen los campos. Habria que revisar como se hace esa parte

--- app/src/main/java/com/example/navigation_login/MainActivity.kt
+++ app/src/main/java/com/example/navigation_login/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.navigation_login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.example.navigation_login.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.BottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId){
+                R.id.opHome -> {
+                    Navigation.findNavController(binding.fragmentContainerView)
+                        .navigate(R.id.initFragment)
+                    true
+                }
+                R.id.opProfile ->{
+                    findNavController(R.id.fragmentContainerView)
+                        .navigate(R.id.firstFragment)
+                    true
+                }
+                else -> false
+            }
+
+        }
     }
 }
